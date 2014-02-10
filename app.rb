ff7fa2d85780dce5ca727f0e5ceba25aa46e7598
Travis Briggs <audiodude@gmail.com>
2014-02-10 04:01:56 +0000

    Add display of links on edit page, use Sinatra content_for.

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+require "sinatra/content_for"
 require 'sqlite3'
 
 get '/' do 
@@ -6,6 +7,8 @@ get '/' do
 end
 
 get '/edit' do
+  db = SQLite3::Database.new 'newslinks.sqlite'
+  @links = db.execute('SELECT id, title, description, url FROM links')
   erb :edit
 end
 
@@ -15,8 +18,8 @@ end
 
 post '/new' do
   db = SQLite3::Database.new 'newslinks.sqlite'
-  db.execute('INSERT INTO links (id, title, description, url) VALUES(?, ?, ?, ?)',
-             [1, params[:title], params[:description], params[:url]]) 
+  db.execute('INSERT INTO links (title, description, url) VALUES(?, ?, ?)',
+             [params[:title], params[:description], params[:url]]) 
   redirect '/edit'
 end
 
