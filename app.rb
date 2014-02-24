4217a0704ef39412860a4a50615d94bd6fb04324
Travis Briggs <audiodude@gmail.com>
2014-02-24 04:08:39 +0000

    Newslinks on the homepage

--- app.rb
+++ app.rb
@@ -3,6 +3,8 @@ require "sinatra/content_for"
 require 'sqlite3'
 
 get '/' do 
+    db = SQLite3::Database.new 'newslinks.sqlite'
+  @links = db.execute('SELECT id, title, description, url FROM links')
   erb :index
 end
 
@@ -16,10 +18,12 @@ get '/new' do
   erb :new
 end
 
+# POST to /new when we are submitting the form for a new newslink
 post '/new' do
   db = SQLite3::Database.new 'newslinks.sqlite'
   db.execute('INSERT INTO links (title, description, url) VALUES(?, ?, ?)',
-             [params[:title], params[:description], params[:url]]) 
-  redirect '/edit'
+             [params[:title], params[:description], params[:url]])
+  # The user has successfully created a newslink, redirect back to the main edit page
+  redirect '/'
 end
 
